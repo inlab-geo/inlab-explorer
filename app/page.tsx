4f67b3a65bf21547af88074abf489b4c7fb5f45a
Denghu-JI <u6535417@anu.edu.au>
2023-07-04 19:31:58 +1000

    build fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import TreeComponent from "../src/components/Rectangle/Rectangle";
 import TopPanel from "@/src/components/topPanel/topPanel";
-import { MyComponent } from "@/src/components/tree/d3";
 import React from "react";
 
 export default function Home() {
